fdaf1ace51654a6317e8d30f2baab9cb07ee85c4
Karl Brewster <mark.moehring@outlook.com>
2022-05-24 19:45:08 -0500

    joined tables

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const mysql = require('mysql2');
 const express = require('express');
+const mysql = require('mysql2');
 const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
@@ -11,20 +11,24 @@ app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: '0Mn1p0+en+',
-      database: 'election'
-    },
-    console.log('Connected to the election database.')
-  );
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '0Mn1p0+en+',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+              AS party_name 
+              FROM candidates 
+              LEFT JOIN parties 
+              ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -40,7 +44,12 @@ app.get('/api/candidates', (req, res) => {
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+              AS party_name 
+              FROM candidates 
+              LEFT JOIN parties 
+              ON candidates.party_id = parties.id 
+              WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
@@ -77,14 +86,19 @@ app.delete('/api/candidate/:id', (req, res) => {
   });
 });
 
-
 // Create a candidate
 app.post('/api/candidate', ({ body }, res) => {
-  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
   if (errors) {
     res.status(400).json({ error: errors });
     return;
   }
+
   const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
     VALUES (?,?,?)`;
   const params = [body.first_name, body.last_name, body.industry_connected];
@@ -101,11 +115,11 @@ app.post('/api/candidate', ({ body }, res) => {
   });
 });
 
-  // Default response for any other request (Not Found)
+// Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
